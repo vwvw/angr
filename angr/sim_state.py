2f4aa040d034ab8ce9202c1e8462306f54a3a917
Thorsten Eisenhofer <thorsten.eisenhofer@rub.de>
2018-05-04 16:55:05 -0700

    Basic support for JNI application
    + SimState
    -> Simultaneously store info about Soot and Vex engine (i.e. memory, arch, registers, ...)
    -> Return these properties based on the current ip
    
    + JavaVM SimOS
    -> Hold an internal native SimOS
    -> With this the native parts (e.g., native memory) of states are initialized and native call states created
    -> Basic matching of exported Java functions in native libraries
    
    + Project
    -> Add Vex engine, when we loaded an JNI application
    
    + Control-Flow Overview
    
    Step 1:
    Soot execution tries to invoke a native function
    
    Step 2:
    - Match Soot method with the symbols exported by the native libaries
    - Create call state with the native function (using the current state als base)
    - Add as return address a hook, where we can switch the engines again
    
    Step 3:
    - Since the ip is not a Soot address anymore, the vex engine will execute the state
    
    Step 4:
    - When the function returns, our hook is executed and we can toggle the state back to Soot mode

--- angr/sim_state.py
+++ angr/sim_state.py
@@ -7,7 +7,7 @@ import logging
 
 import claripy
 import ana
-from archinfo import arch_from_id
+from archinfo import arch_from_id, Arch
 from archinfo.arch_soot import ArchSoot, SootAddressDescriptor
 from .misc.ux import deprecated
 from .misc.plugins import PluginHub, PluginPreset
@@ -51,10 +51,20 @@ class SimState(PluginHub, ana.Storable):
                  add_options=None, remove_options=None, special_memory_filler=None, os_name=None, plugin_preset='default'):
         super(SimState, self).__init__()
         self.project = project
-        self.arch = arch if arch is not None else project.arch.copy() if project is not None else None
+        
 
-        if type(self.arch) is str:
-            self.arch = arch_from_id(self.arch)
+        # Arch
+        if self.javavm_with_jni:
+            self._arch = { "soot": project.arch,  
+                           "vex" : project.simos.native_simos.arch }
+            # This flag indicates whether the current ip is a native address or a soot address descriptor.
+            # Background: We cannot solely rely on the ip, because the registers (storing the ip) are part 
+            # of the plugins that are getting "switched".
+            self.ip_is_soot_addr = False
+        else: 
+            self._arch = arch if arch is not None else project.arch.copy() if project is not None else None
+            if type(self._arch) is str:
+                self._arch = arch_from_id(self._arch)
 
         # the options
         if options is None:
@@ -257,6 +267,12 @@ class SimState(PluginHub, ana.Storable):
         else:
             raise SimStateError("Unsupported type '%s' in SimState.options.setter()." % type(v))
 
+    def arch(self):
+        if self.javavm_with_jni:
+            return self._arch['soot'] if self.ip_is_soot_addr else self._arch['vex']
+        else:
+            return self._arch
+   
     #
     # Plugin accessors
     #
@@ -454,6 +470,9 @@ class SimState(PluginHub, ana.Storable):
         c_plugins = self._copy_plugins()
         state = SimState(project=self.project, arch=self.arch, plugins=c_plugins, options=self.options.copy(),
                          mode=self.mode, os_name=self.os_name)
+
+        if self.javavm_with_jni:
+            state.ip_is_soot_addr = self.ip_is_soot_addr
 
         state.uninitialized_access_handler = self.uninitialized_access_handler
         state._special_memory_filler = self._special_memory_filler

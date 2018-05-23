32544cd5e05d8616a7dec650a62ade868a654dc3
Thorsten Eisenhofer <thorsten.eisenhofer@rub.de>
2018-05-23 16:27:30 -0700

    Add logger to SimState

--- angr/sim_state.py
+++ angr/sim_state.py
@@ -3,8 +3,6 @@ import itertools
 import contextlib
 import weakref
 
-import logging
-
 import claripy
 import ana
 from archinfo import arch_from_id, Arch
@@ -12,6 +10,9 @@ from archinfo.arch_soot import ArchSoot, SootAddressDescriptor
 from .misc.ux import deprecated
 from .misc.plugins import PluginHub, PluginPreset
 from .sim_state_options import SimStateOptions
+
+import logging
+l = logging.getLogger("angr.sim_state")
 
 def arch_overrideable(f):
     @functools.wraps(f)
@@ -272,7 +273,21 @@ class SimState(PluginHub, ana.Storable):
             return self._arch['soot'] if self.ip_is_soot_addr else self._arch['vex']
         else:
             return self._arch
-   
+
+    @property
+    def javavm(self):
+        """
+        Returns if this state can be used with a JavaVM SimOS.
+        """
+        return self.project and isinstance(self.project.arch, ArchSoot)
+
+    @property
+    def javavm_with_jni(self):
+        """
+        Returns if this state can be used with a JavaVM SimOS with JNI support.
+        """
+        return self.project and isinstance(self.project.arch, ArchSoot) and self.project.simos.jni_support
+
     #
     # Plugin accessors
     #
